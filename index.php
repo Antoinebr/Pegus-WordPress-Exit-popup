52810ec0ef48118519ad9e533efc3e28fcd5e239
Antoine <contact@antoinebrossault.com>
2015-04-06 16:55:21 +0200

    supression du texte d'index.php et suppresion de test.php

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
 <?php // Silence is golden ?>
-
-<H1> Si vous voyez ce msg vous n'êtes pas au bon endroit </H1>
-
-
-<p> Pandi nou :) </p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
